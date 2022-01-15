f8f73c3ca906ea15e709450ccdb89cd666235fcd
WalterJunior <walterjuniordossanto@gmail.com>
2022-01-14 21:22:30 -0300

    Estrutura condicional em JS

---
+++ script.js
@@ -0,0 +1,24 @@
+let age = 20;
+let human = true;
+if(age > 18){
+    console.log("Você é maior de idade")
+    console.log(human)
+}
+let birthday = "Dezember";
+if(birthday = false){
+    console.log("O seu mês é em Janeiro")
+}else{
+    console.log("O seu mês é em Dezembro")
+}
+let itsme = "Walter"
+if( itsme = false){
+    console.log("Seu nome começa com R")
+}else{
+    console.log("Seu nome não começa com R")
+}
+let surname = "Santos";
+if(surname = false){
+    console.log("seu sobrenome tem mais de 6 letras OU seu nome começar com a letra E")
+}else{
+    console.log("seu sobrenome não tem mais de 6 letras OU seu nome não começar com a letra E")
+}
